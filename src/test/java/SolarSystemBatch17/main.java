ae9f2b21edc2dbbea7f4673a565d83ed80590146
tamimkmz <tamim.kmz@gmail.com>
2023-11-03 10:46:11 -0700

    this is the structure of solar system

---
+++ src/test/java/SolarSystemBatch17/main.java
@@ -0,0 +1,14 @@
+package SolarSystemBatch17;
+
+import java.nio.channels.UnresolvedAddressException;
+
+public class main {
+    public static void main(String[] args) {
+        solarSystemBasics solarsys=new solarSystemBasics();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+
+    }
+}
---
+++ src/test/java/SolarSystemBatch17/solarSystemBasics.java
@@ -0,0 +1,11 @@
+package SolarSystemBatch17;
+
+public class solarSystemBasics {
+
+        public String Sun;
+        public String Moon;
+        public String Stars;
+        public String Planets;
+
+    }
+
